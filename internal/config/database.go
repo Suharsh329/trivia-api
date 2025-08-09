df9a0543e6f730d6a4f707f9d9a98d9be73e2be2
Suharsh Nagasampagi <suharsh329@gmail.com>
2025-08-09 00:09:28 -0700

    Update path

--- internal/config/database.go
+++ internal/config/database.go
@@ -3,13 +3,23 @@ package config
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 // Connect to a database
 func ConnectDatabase() *sql.DB {
-	db, err := sql.Open("sqlite3", "internal/database/trivia.db")
+	var dbPath string
+
+	appEnv := os.Getenv("APP_ENV")
+	if appEnv == "local" {
+		dbPath = "internal/database/trivia.db"
+	} else {
+		dbPath = "data/trivia.db"
+	}
+
+	db, err := sql.Open("sqlite3", dbPath)
 	if err != nil {
 		log.Fatalf("Error connecting to the database: %v", err)
 	}
