092636b448d122f4d5e26506c38e7926b22376fc
Suharsh Nagasampagi <suharsh329@gmail.com>
2025-10-05 16:14:10 +1100

    Add health route

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,9 @@
 use App\Http\Controllers\GameController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/health', function () {
+    return response()->json(['status' => 'ok']);
+});
+
 Route::post('/games/random', [GameController::class, 'createRandomGame']);
 
